c48efd10905d765adfa00220098bb693ea7cc85a
U.Berke Yıldız <148607805+ulyildiz@users.noreply.github.com>
2025-04-08 15:37:49 +0300

    .

---
+++ SimpleErrorHandle/SimpleErrorHandle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using System;
+class Program
+{
+    public static void Main()
+    {
+        string input;
+
+        while (true)
+        {
+            Console.WriteLine("Enter number:");
+            input = Console.ReadLine();
+            try
+            {
+                decimal.Parse(input);
+                Console.WriteLine("Valid input received.");
+                Console.WriteLine($"You entered: {input}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
