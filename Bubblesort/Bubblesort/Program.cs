2079eee673f9c3ff12c82195994970a18ca319e7
U.Berke Yıldız <148607805+ulyildiz@users.noreply.github.com>
2025-04-08 15:20:32 +0300

    .

---
+++ Bubblesort/Bubblesort/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = 5;
+int m = 3;
+
+if (m <= n)
+{
+    int result1 = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result1 *= i;
+    }
+
+    int result2 = 1;
+    for (int j = 1; j <= n-m; j++)
+    {
+        result2 *= j;
+    }
+
+    Console.WriteLine(result1 / result2);
+}
---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public static double Func(double x)
+    {
+        x = x * Math.PI / 180.0;
+        return (Math.Tan(x/2)+(3*Math.Sin(x)*Math.Cos(2*x)));
+    }
+
+    public static void Main()
+    {
+        try
+        {
+            Console.WriteLine(Func(double.Parse(Console.ReadLine())));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }   
+}
